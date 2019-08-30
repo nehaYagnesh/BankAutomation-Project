e6120310899ad32506e7afe01d906bb4018c52b8
Neha Parekh <nlimbachiya207@gmail.com>
2019-08-30 15:36:47 -0400

    TestCase 5 : Using @DataProvider instead of excel and verified whether managerid is displayed on successful login

---
+++ src/test/java/Browser/Util1.java
@@ -0,0 +1,21 @@
+package Browser;
+
+public class Util1 {
+	
+	public static final String FIREFOX_PATH = "C:\\Users\\nlimb\\Eclipse_nSel\\Bank_Automation\\drivers\\firefoxDriver\\geckodriver.exe";
+	
+	public static final String BASE_URL ="http://www.demo.guru99.com/";
+	
+	//Valid Login Credentials
+	public static final String USER_NAME ="mngr219136";
+	public static final String PASSWORD = "ujYvEbY";
+	
+	public static final int WAIT_TIME = 30;
+	public static final String EXPECTED_TITLE = "Guru99 Bank Manager HomePage";
+	public static final String EXPECTED_ERROR ="User or Password is not valid";
+	
+	public static final String PATTERN = ":";
+	public static final String FIRST_PATTERN = "mngr";
+	public static final String SECOND_PATTERN ="[0-9]+";
+	
+}
